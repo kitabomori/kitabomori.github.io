47a5dd483d7c0063b6f6346cf0d141d14784f5c6
kitabomori <24170014@lums.edu.pk>
2026-07-09 15:19:50 +0500

    design Change

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,7 @@ module.exports = {
         },
       },
       fontFamily: {
-        sans: ['Inter', 'sans-serif'],
+        sans: ['Literata', 'serif'],
         urdu: ['"Noto Nastaliq Urdu"', 'serif'],
       },
     },
